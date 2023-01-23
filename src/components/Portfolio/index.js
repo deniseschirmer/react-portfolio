882fa14ee50c66aa1324965cf2fa5c76cfb4f544
Denise Schirmer <denise_liima@hotmail.com>
2023-01-23 07:57:51 -0300

    teste

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -31,31 +31,24 @@ const Portfolio = () => {
   const renderPortfolio = (portfolio) => {
     return (
       <div className="images-container">
-        {portfolio
-          ? portfolio.map((port, idx) => {
-              return (
-                <div className="image-box" key={idx}>
-                  <img
-                    src={port.image}
-                    className="portfolio-image"
-                    alt="portfolio"
-                  />
-                  <div className="content">
-                    <p className="title">{port.name}</p>
-                    <h4 className="description">{port.description}</h4>
-                    <button
-                      className="btn"
-                      onClick={() => window.open(port.url)}
-                    >
-                      View
-                    </button>
-                  </div>
-                </div>
-              )
-            })
-          : alert('carregando ')} 
-
-          <h1>Fora</h1>
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.image}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+              <div className="content">
+                <p className="title">{port.name}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  View
+                </button>
+              </div>
+            </div>
+          )
+        })}
       </div>
     )
   }
@@ -70,7 +63,26 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        <div>{renderPortfolio(portfolio)}</div>
+        <div className="images-container">
+          {portfolio && portfolio.map((port, idx) => {
+            return (
+              <div className="image-box" key={idx}>
+                <img
+                  src={port.image}
+                  className="portfolio-image"
+                  alt="portfolio"
+                />
+                <div className="content">
+                  <p className="title">{port.name}</p>
+                  <h4 className="description">{port.description}</h4>
+                  <button className="btn" onClick={() => window.open(port.url)}>
+                    View
+                  </button>
+                </div>
+              </div>
+            )
+          })}
+        teste</div>
       </div>
       <Loader type="pacman" />
     </>
